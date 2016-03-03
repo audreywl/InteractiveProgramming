7a77f15bba2bf648f26684195e3292948df61c6b
KatieButler <katiethetraveller@gmail.com>
2016-03-03 18:31:56 -0500

    "before I pulled Audrey's stuff"

--- MP4.py
+++ MP4.py
@@ -1,1 +1,2 @@
-"""This is Katie and Audrey's Software Design Mini-Project 4. It uses pygame to load a music visualizer that you can jump on."""
+"""This is Katie and Audrey's Software Design Mini-Project 4. It uses pygame to load a music visualizer that you can jump on" 
+
